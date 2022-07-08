557601dc220286bc95cf61e18bdfbacb03ba55d6
Evgenii Khramkov <evgenii.khramkov@evgenii.khramkov>
2022-07-08 10:42:04 +0300

    update scripts

---
+++ scripts/setup.sh
@@ -0,0 +1,12 @@
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install iterm
+brew install --cask iterm2
+
+# install vscode
+brew install --cask visual-studio-code
+
+# install JB mono font
+brew tap homebrew/cask-fonts
+brew install --cask font-jetbrains-mono
