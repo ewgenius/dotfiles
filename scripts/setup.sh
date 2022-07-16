08a813ca96b7fa5762ad9756301de0c336ba4e68
Evgenii Khramkov <ewgeniux@gmail.com>
2022-07-16 18:48:23 +0300

    update deps

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,19 @@
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# install iterm
+# Utilities
+brew install --cask raycast
+brew install --cask cleanshot # https://licenses.maketheweb.io/
+brew install --cask google-drive
+brew install --cask surfshark
+brew install --cask bitwarden
+brew install --cask brave-browser
+brew install --cask notion
+brew install --cask telegram
+
+# Dev Tools
+brew tap homebrew/cask-fonts && brew install --cask font-jetbrains-mono
 brew install --cask iterm2
-
-# install vscode
 brew install --cask visual-studio-code
-
-# install JB mono font
-brew tap homebrew/cask-fonts
-brew install --cask font-jetbrains-mono
-
-# raycast
-brew reinstall --cask raycast
-
-# gh cli
 brew install --cask gh
+brew install nvm
